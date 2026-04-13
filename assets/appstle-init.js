d153de63a3b7aefcf42d8e4bdb16598e51dbd41f
ammadibnearshad <dev.lead@markloops.net>
2026-04-13 13:05:30 +0500

    init

---
+++ assets/appstle-init.js
@@ -0,0 +1,206 @@
+(function (window, k) {
+    if (!window.AppstleIncluded && (!urlIsProductPage() || 'V1' === 'V2')) {
+      window.AppstleIncluded = true;
+      appstleLoadScript = function (src, callback) {
+        var script = document.createElement("script");
+        script.charset = "utf-8";
+            script.defer = true;
+        script.src = src;
+        script.onload = script.onreadystatechange = function () {
+          if (!script.readyState || /loaded|complete/.test(script.readyState)) {
+            script.onload = script.onreadystatechange = null;
+            script = k;
+            callback && callback();
+          }
+        };
+            document.getElementsByTagName("body")[0].appendChild(script)
+      };
+
+
+      appstleLoadScript("https://cdn.shopify.com/s/files/1/0268/6104/3746/t/23/assets/appstle-subscription.js?v=1679343408");
+
+
+      window.RS = Window.RS || {};
+      RS.Config = {
+        "selectors": {
+            "payment_button_selectors": "form[action$='/cart/add'] .shopify-payment-button",
+            "subscriptionLinkSelector": "",
+            "atcButtonPlacement": "BEFORE",
+            "subscriptionLinkPlacement": "BEFORE",
+            "cartRowSelector": "",
+            "cartLineItemSelector": "",
+            "cartLineItemPerQuantityPriceSelector": "",
+            "cartLineItemTotalPriceSelector": "",
+            "cartLineItemSellingPlanNameSelector": "",
+            "cartSubTotalSelector" : "",
+            "cartLineItemPriceSelector": "",
+        },
+        "enableCartWidgetFeature": "false",
+        "useUrlWithCustomerId": "true",
+        "atcButtonSelector": "",
+        "moneyFormat": "{% raw %}{{amount}}${% endraw %}",
+        "oneTimePurchaseText": "Recevoir la lettre une fois",
+        "shop": "raplaplamtl.myshopify.com",
+        "deliveryText": "delivery",
+        "purchaseOptionsText": "",
+        "manageSubscriptionButtonText": "Manage Subscription",
+        "subscriptionOptionText": "S'abonner (et \u00E9conomiser !)",
+        "sellingPlanSelectTitle": "FR\u00C9QUENCE DES LIVRAISONS",
+        "subscriptionPriceDisplayText": "",
+        "tooltipTitle": "Des d\u00E9tails sur l'abonnement",
+        "showTooltipOnClick": "false",
+        "tooltipDesctiption": "<strong>C'est toi qui choisi \u263A\uFE0E<\/strong><br\/><br\/>Saute, reprogramme ou annule l'envoi de la lettre \u00E0 tout moment, en fonction de tes besoins. Suite au premier paiement, le montant d\u00FB pour l'abonnement sera pr\u00E9lev\u00E9 tous les 15 du mois. ",
+        "tooltipDescriptionOnPrepaidPlan": "<b>Prepaid Plan Details<\/b><\/br> Total price: {{totalPrice}} ( Price for every delivery: {{pricePerDelivery}})",
+        "tooltipDescriptionOnMultipleDiscount": "<b>Discount Details<\/b><\/br> Initial discount is {{discountOne}} and then {{discountTwo}}",
+        "tooltipDescriptionCustomization": "{{{defaultTooltipDescription}}} <\/br>  {{{prepaidDetails}}} <\/br> {{{discountDetails}}}",
+        "orderStatusManageSubscriptionTitle": "Abonnement",
+        "orderStatusManageSubscriptionDescription": "Acc\u00E9de \u00E0 ton compte pour consulter et g\u00E9rer tes abonnements. Merci d'utiliser le m\u00EAme courriel que celui que tu as utilis\u00E9 pour acheter l'abonnement.",
+        "orderStatusManageSubscriptionButtonText": "G\u00E8re ton abonnement",
+        "subscriptionOptionSelectedByDefault" : false,
+        "totalPricePerDeliveryText" : "{{prepaidPerDeliveryPrice}}\/delivery",
+        "memberOnlySellingPlansJson": {},
+        "nonMemberOnlySellingPlansJson": {},
+        "widgetEnabled": true,
+        "showTooltip" : true,
+        "sortByDefaultSequence": false,
+        "showSubOptionBeforeOneTime": false,
+        "showStaticTooltip": false,
+        "showAppstleLink": true,
+        "sellingPlanTitleText" : "{{sellingPlanName}} ({{sellingPlanPrice}}\/par livraison)",
+        "oneTimePriceText" : "{{price}}",
+        "selectedPayAsYouGoSellingPlanPriceText" : "{{price}}",
+        "selectedPrepaidSellingPlanPriceText" : " {{totalPrice}}",
+        "selectedDiscountFormat" : "SAVE {{selectedDiscountPercentage}}",
+        "manageSubscriptionBtnFormat" : "<a href='apps\/subscriptions' class='appstle_manageSubBtn' ><button class='btn' style='padding: 2px 20px'>Manage Subscription<\/button><a><br><br>",
+        "manageSubscriptionUrl" : "apps\/subscriptions",
+        "appstlePlanId": 161,
+        "showCheckoutSubscriptionBtn": true,
+        "disableLoadingJquery": false,
+        "widgetEnabledOnSoldVariant": "false",
+        "switchRadioButtonWidget": true,
+        "appstlePlanName": "FREE",
+        "appstlePlanFeatures": {
+	"subscriptionOrderAmount": 500.0,
+	"analytics": true,
+	"enableSubscriptionManagement": true,
+	"enableDunningManagement": true,
+	"enableCustomerPortalSettings": true,
+	"enableShippingProfiles": true,
+	"enableProductSwapAutomation": false,
+	"enableAdvancedSellingPlans": false,
+	"enableSummaryReports": true,
+	"enableCustomEmailDomain": false,
+	"enableWidgetPlacement": false,
+	"enableIntegrations": true,
+	"enableSmsAlert": false,
+	"enableCustomEmailHtml": false,
+	"enableCancellationManagement": false,
+	"enableBundling": false,
+	"enableAutomation": false,
+	"enableQuickActions": false,
+	"enableExternalApi": false,
+	"enableCartWidget": false,
+	"enableAutoSync": false,
+	"webhookAccess": false,
+	"accessWidgetDesignOptions": false,
+	"accessSubscriptionActivityLogs": true,
+	"accessBuildABox": false,
+	"accessResendEmail": false,
+	"accessKlaviyoContactSync": false,
+	"accessOneTimeProductUpsells": false,
+	"accessAdvanceSubscriptionPlanOptions": false,
+	"accessSplitContract": false,
+	"accessDiscountOnCancellationAttempt": false,
+	"accessQuickCheckout": false,
+	"accessSubscriberLoyaltyFeatures": false,
+	"accessBundling": false,
+	"accessManualSubscriptionCreation": false,
+	"accessAppstleMenu": false
+},
+        "formMappingAttributeName": "",
+        "formMappingAttributeSelector": "",
+        "quickViewModalPollingSelector": "",
+        "scriptLoadDelay": "0",
+        "formatMoneyOverride": "false",
+        "appstle_app_proxy_path_prefix": "apps\/subscriptions",
+        "updatePriceOnQuantityChange": "",
+        "widgetParentSelector": "",
+        "quantitySelector": "",
+        "enableAddJSInterceptor": "false",
+        "reBuyEnabled": "false",
+        "loyaltyDetailsLabelText": "",
+        "loyaltyPerkDescriptionText": "",
+        "widgetTemplateHtml": `{% raw %}{% endraw %}`,
+        "bundle": {},
+        "labels": "{\"appstle.subscription.wg.yearsFrequencyTextV2\":\"ann\u00E9es\",\"appstle.subscription.wg.weekFrequencyTextV2\":\"semaine\",\"appstle.subscription.wg.oneTimePurchaseTextV2\":\"One Time Purchase\",\"appstle.subscription.wg.loyaltyPerkDescriptionTextV2\":\"{{#isDiscountTypeFreeProduct}}<div style='display: flex;'><div style='height: 60px; width: 60px; flex-shrink: 0; margin-right: 10px;'><img style='width: 100%' src={{{featured_image}}}><\/img><\/div><div>After {{{billingCycleBlock}}} orders,<span style='color: #ffc000;font-weight: 700;';> get a FREE {{freeProductName}} <\/span><\/div><div>{{\/isDiscountTypeFreeProduct}}{{#isDiscountTypePercentage}}After <span class='appstle-loyalty-billing-cycle'><span class='appstle-loyalty-billing-cycle-count'>{{{billingCycleBlock}}}<\/span> order<\/span>, <span class='appstle-loyalty-discount'>get <span style='color: #ffc000;font-weight: 700;';>{{{discount}}}% OFF your entire order<\/span><\/span>.{{\/isDiscountTypePercentage}}{{#isDiscountTypeShipping}}After <span class='appstle-loyalty-billing-cycle'><span class='appstle-loyalty-billing-cycle-count'>{{{billingCycleBlock}}}<\/span> order<\/span>, <span class='appstle-loyalty-discount'>get <span style='color: #ffc000;font-weight: 700;';>shipping at {{{formatDiscountedPrice}}}<\/span><\/span>.{{\/isDiscountTypeShipping}}{{#isDiscountTypeFixed}}After <span class='appstle-loyalty-billing-cycle'><span class='appstle-loyalty-billing-cycle-count'>{{{billingCycleBlock}}}<\/span> order<\/span>, <span class='appstle-loyalty-discount'>get <span style='color: #ffc000;font-weight: 700;';>{{{formatDiscountedPrice}}} OFF your entire order<\/span><\/span>.{{\/isDiscountTypeFixed}}\",\"appstle.subscription.wg.unsubscribeFrequencyTextV2\":\"unsubscribe\",\"appstle.subscription.wg.weeksFrequencyTextV2\":\"semaines\",\"appstle.subscription.wg.oneTimeFrequencyTextV2\":\"une fois\",\"appstle.subscription.wg.dayFrequencyTextV2\":\"jour\",\"appstle.subscription.wg.allowFulfilmentCountViaPropertiesV2\":\"false\",\"appstle.subscription.wg.monthsFrequencyTextV2\":\"mois\",\"appstle.subscription.wg.deliveryEveryFrequencyTextV2\":\"Delivery Every\",\"appstle.subscription.wg.subscribeAndSaveInitalV2\":\"Subscribe & save\",\"appstle.subscription.wg.offFrequencyTextV2\":\"de moins\",\"appstle.subscription.wg.yearFrequencyTextV2\":\"ann\u00E9e\",\"appstle.subscription.wg.daysFrequencyTextV2\":\"jours\",\"appstle.subscription.wg.subscribeAndSaveSuccessV2\":\"Subscribe success\",\"appstle.subscription.wg.monthFrequencyTextV2\":\"mois\",\"appstle.subscription.wg.selectDeliverOptionV2\":\"select deliver option\"}",
+        "css": {
+            "appstle_subscription_widget": {
+                "margin-top": "" ,
+                "margin-bottom": "",
+            },
+
+            "appstle_subscription_wrapper": {
+                "border-width": "",
+                "border-color": "",
+            },
+
+            "appstle_circle": {
+                "border-color": "",
+            },
+
+            "appstle_dot": {
+                "background-color": "",
+            },
+
+            "appstle_select": {
+                "padding-top": "",
+                "padding-bottom": "",
+                "padding-left": "",
+                "padding-right": "",
+                "border-width": "",
+                "border-style": "",
+                "border-color": "",
+                "border-radius": "",
+            },
+
+            "tooltip_subscription_svg": {
+                "fill": "",
+            },
+
+            "appstle_tooltip": {
+                "color": "",
+                "background-color": "",
+            },
+
+            "appstle_tooltip_border_top_color": {
+                "border-top-color": "",
+            },
+
+            "appstle_subscription_final_price": {
+                "color": "",
+            },
+            "appstle_widget_text_color": {
+                "color": "",
+            },
+            "appstle_selected_background": {
+                "background": "transparent",
+            },
+            "customCSS": "",
+            "customerPortalCss": "",
+            "priceSelector": "",
+            "landingPagePriceSelector": "",
+            "quickViewClickSelector": "",
+            "badgeTop": "",
+            "pricePlacement": "BEFORE"
+        }
+      };
+
+    }
+
+    function urlIsProductPage() {
+    // return null != decodeURIComponent(window.location.pathname).match(/\/products\/(([a-zA-Z0-9]|[\-\.\_\~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[\ud83c\ud83d\ud83e][\ud000-\udfff]){1,})\/?/)
+    return decodeURIComponent(window.location.pathname).includes('/products/');
+    }
+  }
+)(window);
+
